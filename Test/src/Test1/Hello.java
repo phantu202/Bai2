5306a8bef9c94bac7cab2f2f39f964286a7d78ab
phantu202“git config --global user.email phantu202@gmail.com“ <125955700+phantu202@users.noreply.github.com>
2023-02-23 13:49:06 +0700

    first commit

---
+++ Test/src/Test1/Hello.java
@@ -0,0 +1,19 @@
+package Test1;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 Scanner sc = new Scanner(System.in);
+	 int x = sc.nextInt();
+	 int y = sc.nextInt();
+	 
+	 System.out.println(x + y );
+	 System.out.println("hello viet nam");
+	 
+		
+	}
+
+}
